bee86540be5d4ead973375ac3f9bb6e9721b93ba
Nick <119486388+CryPaw@users.noreply.github.com>
2023-03-15 17:30:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let yearOfBirth = Number(prompt('Ваш рік народження'));
+let yearOfBirth = prompt('Ваш рік народження');
 let selectedCity = prompt('В якому місті проживаєте');
 let selectedSport = prompt('Ваш улюблений вид спорту');
 let messageAboutCity;
@@ -8,70 +8,59 @@ let messageAboutАge;
 if (yearOfBirth > 2023)
 {messageAboutАge = 'Ви не можете жити у майбутньому'}
 else if (yearOfBirth < 0)
-{messageAboutАge = 'Ви ввели від`ємне число'}
+{messageAboutАge = "Ви ввели від'ємне число"}
+else if (yearOfBirth === null)
+{messageAboutАge = "Шкода, що Ви не захотіли ввести свій вік"}
+else if (yearOfBirth === '')
+{messageAboutАge = "Шкода, що Ви не захотіли ввести свій вік"}
+else if (yearOfBirth <= 1900)
+{messageAboutАge = "Люди стільки не живуть"}
+else if (isNaN(yearOfBirth) === true)
+{messageAboutАge = "Шкода, що Ви не захотіли ввести свій вік"}
 else {
-switch (yearOfBirth){
-case (null):
-case (''):
-    messageAboutАge = "Шкода, що Ви не захотіли ввести свій вік";
-break;
-default:
-    messageAboutАge = `Вам ${2023 - yearOfBirth} років`;
-}
+    messageAboutАge = `Вам ${2023 - parseInt(yearOfBirth, 10)} років`;
 }
 
-switch (selectedCity){
-    case(null):
+if (selectedCity === null)
+{messageAboutCity = "Шкода, що Ви не захотіли ввести своє місто"}
+else {
+switch (selectedCity.toLowerCase()){
     case (''):
         messageAboutCity = "Шкода, що Ви не захотіли ввести своє місто";
         break;
-    case "Київ":
-    case "Kyiv":
     case "київ":
-    case "kyiv":
         messageAboutCity = "Ти живеш у столиці України";
         break;
-    case "Вашингтон":
-    case "Washington":
     case "вашингтон":
-    case "washington":
         messageAboutCity = "Ти живеш у столиці Сполучених Штатів Америки";
         break;
-    case "Київ":
-    case "Kyiv":
-    case "київ":
-    case "kyiv":
+    case "лондон":
         messageAboutCity = "Ти живеш у столиці Англії";
         break;
     default:
         messageAboutCity = `Ти живеш у місті ${selectedCity}`;
 }
+}
 
-switch (selectedSport){
-    case(null):
+if (selectedSport === null)
+{messageAboutSport = "Шкода, що Ви не захотіли ввести свій улюблений вид спорту"}
+else {
+switch (selectedSport.toLowerCase()){
     case (''):
         messageAboutSport = "Шкода, що Ви не захотіли ввести свій улюблений вид спорту";
         break;
-    case "Бокс":
-    case "Box":
     case "бокс":
-    case "box":
         messageAboutSport = "Круто! Хочеш стати як Майк Тайсон?";
         break;
-    case "Баскетбол":
-    case "Basketball":
     case "баскетбол":
-    case "basketball":
         messageAboutSport = "Круто! Хочеш стати як Майк Джордан?";
-        break;
-    case "Футбол":
-    case "Football":
-    case "футбол":
-    case "football":
+        break; 
+    case "футбол": 
         messageAboutSport = "Круто! Хочеш стати як Криштиану Рональдо?";
         break;
     default:
         messageAboutSport = `${selectedSport} гарний вид спорту`;
+}
 }
 
 alert  (messageAboutАge + ' \n' + messageAboutCity + ' \n' + messageAboutSport);
